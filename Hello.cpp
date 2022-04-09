718b93185a2193eb342a219802aa8a499f924bd0
HGTNewCoder <thinh.hagia22@gmail.com>
2022-04-09 22:46:34 +0700

    Update

---
+++ Hello.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void sort(int n, int arr[]){
+    int temp = 0;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (arr[i] < arr[j]){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for (int i = 1; i < n; i++){
+        cout<<arr[i];
+    }
+}
+// void outp(arr){
+
+// }
+int main(){
+    int n = 10;
+    int array[n] = {1, 3, 5, 7, 9, 2, 4, 6, 8};
+
+    sort(n, array);
+    return 0;
+}
